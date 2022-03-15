469312b38eb60c5281aa21d9f9f008c6f97f29ab
qnghe <1303280520@qq.com>
2022-03-15 17:31:00 +0800

    feat: 音频转写页面修改

--- src/pages/audioToText/Index.vue
+++ src/pages/audioToText/Index.vue
@@ -1,33 +1,44 @@
 <template>
-  <div class="audio-text">
-    <div>
+    <div class="audio-text">
         <el-upload
             class="upload-demo"
             drag
             action="https://jsonplaceholder.typicode.com/posts/"
             multiple
-            on-progress="uploading"
+            :on-progress="uploading"
         >
             <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-            <div class="el-upload__text">
-                文件拖来或<em>点击上传</em>
-            </div>
+            <div class="el-upload__text">文件拖来或<em>点击上传</em></div>
             <template #tip>
                 <div class="el-upload__tip">只能上传音频文件，小于500MB</div>
             </template>
         </el-upload>
+        <el-card class="download-box" shadow="hover">
+            <h4>文本文件</h4>
+            <el-icon class="icon-download"><download /></el-icon>
+        </el-card>
     </div>
-  </div>
 </template>
 <script setup>
-import { UploadFilled } from "@element-plus/icons-vue";
+import { UploadFilled, Download } from "@element-plus/icons-vue";
 const uploading = (event, file, fileList) => {
-    console.log(event);
-}
+  console.log(event);
+};
 </script>
 <style scoped>
 .audio-text {
   margin-top: 80px;
   text-align: center;
+}
+.download-box {
+    margin: 48px auto 0;
+    width: 360px;
+    cursor: pointer;
+}
+.download-box:hover {
+    color: rgb(70, 170, 252);
+}
+.icon-download {
+    font-size: 48px;
 }
 </style>
